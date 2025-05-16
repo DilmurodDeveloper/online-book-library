0ccb3e72dcc831bf01624a8a9473310e4172ac17
Dilmurod Madirimov <madirimovdilmurod2@gmail.com>
2025-05-17 00:34:25 +0500

    FEAT: Add shared models - CategoryEnum and ApiResponse

---
+++ Shared/Enums/CategoryEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineBookLibrary.Shared.Enums
+{
+    public enum CategoryEnum
+    {
+        Fiction = 1,
+        NonFiction = 2,
+        Science = 3,
+        History = 4,
+        Technology = 5
+    }
+}
